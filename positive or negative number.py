e2d74558e99092e4c0d2ba8dc4ff5aa3a04ae1da
jeeva1212 <jeeva11041995@gmail.com>
2024-06-22 15:16:24 +0530

    python prog

---
+++ positive or negative number.py
@@ -0,0 +1,8 @@
+num = float(input("Enter a number:"))
+#input:1.2
+if num>0:
+    print("positive number")
+elif num==0:
+    print("zero")
+else:
+    print("negative number")
---
+++ sum of two numbers.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter a number1:"))
+num2 = int(input("Enter a number2:"))
+print("sum of given number is:",num1+num2)
+
+"""a=5
+b=10
+c=a+b
+print(c)"""
